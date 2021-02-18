45a9bb5f538f3704b2210e4e1d6e4ffded5cd9f0
Luc Gotink <L.h.gotink@gmail.com>
2021-02-18 14:38:07 +0000

    added accordion functionality for cards

---
+++ main.js
@@ -0,0 +1,15 @@
+    $('.accordion').click(function() {
+        var panel = $(this).closest('.card-title').children('.panel');
+        if ($(panel).css('display') == 'flex') {
+                $(panel).css('display', 'none');
+                $(this).text("Maaltijden lijst");
+            } else {
+                $(panel).css('display', 'flex');
+                $(this).text("Ik wil deze dag geen maaltijden");
+        }
+    });
+
+
+// .css('display','flex'))
+// .css('display','none');
+// .css('display', 'flex');
