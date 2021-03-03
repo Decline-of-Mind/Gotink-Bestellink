b0f3060369f980edc431caf216e8721985b67c02
Luc Gotink <L.h.gotink@gmail.com>
2021-03-03 17:13:17 +0000

    Basic layout and javascript functionality added to hotmeals ordering. Layout for ipad and mobile still pending

--- main.js
+++ main.js
@@ -8,3 +8,14 @@
                 $(this).text("Ik wil deze dag geen maaltijden");
         }
     });
+    // WARME MAALTIJD SERVICE HEEFT ANDERE OPBOUW. Line 13, Siblings IPV children
+        $('.accordion').click(function() {
+        var panel = $(this).closest('.card-title').siblings('.panel');
+        if ($(panel).css('display') == 'flex') {
+                $(panel).css('display', 'none');
+                $(this).text("Maaltijden lijst");
+            } else {
+                $(panel).css('display', 'flex');
+                $(this).text("Ik wil deze dag geen maaltijden");
+        }
+    });
